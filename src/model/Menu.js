e95c309734e5a44a22e33ddb1e649ba836b579e3
Taeyooooon <eric568@naver.com>
2023-11-13 16:33:13 +0900

    fix: 전체 주문 메뉴 20개 이상일 경우 예외처리

--- src/model/Menu.js
+++ src/model/Menu.js
@@ -3,11 +3,11 @@ import Validate from '../utils/Validate.js';
 
 class Menu {
   #menuList = [];
-  #totalCount = 0;
 
   constructor(menus) {
     this.#validate(menus);
     this.#setMenus(menus);
+    this.#validateTotalCount();
   }
 
   #validate(menus) {
@@ -19,47 +19,6 @@ class Menu {
     const [menuName, count] = menu.split('-');
     this.#validateMenu(menuName);
     this.#validateCount(count);
-  }
-
-  get totalPrice() {
-    return this.#menuList.reduce((total, { count, price }) => total + price * count, 0);
-  }
-
-  #getMenuCategory(menuName) {
-    const menuCategory = Object.keys(MENU_LIST).find((category) =>
-      MENU_LIST[category].some(({ name }) => name === menuName),
-    );
-
-    return menuCategory;
-  }
-
-  #getMenuPrice(menuName) {
-    const menu = Object.values(MENU_LIST)
-      .flat()
-      .find((item) => item.name === menuName);
-
-    return menu.price;
-  }
-
-  #setMenus(menus) {
-    const menuList = menus.split(',').map((each) => each.split('-'));
-    menuList.forEach(([menuName, count]) => {
-      const list = {
-        name: menuName,
-        count,
-        category: this.#getMenuCategory(menuName),
-        price: this.#getMenuPrice(menuName),
-      };
-      this.#validateDuplicateMenu(menuName);
-
-      this.#menuList.push(list);
-    });
-  }
-
-  #validateDuplicateMenu(menuName) {
-    if (this.#menuList.some(({ name }) => name === menuName)) {
-      throw new Error(ERROR.orderDuplicate);
-    }
   }
 
   #validateMenu(menuName) {
@@ -80,6 +39,55 @@ class Menu {
     if (Number(count) > EVENT_RULES.maxOrder) {
       throw new Error(ERROR.order);
     }
+  }
+
+  #setMenus(menus) {
+    const menuList = menus.split(',').map((each) => each.split('-'));
+    menuList.forEach(([menuName, count]) => {
+      const list = {
+        name: menuName,
+        count: Number(count),
+        category: this.#getMenuCategory(menuName),
+        price: this.#getMenuPrice(menuName),
+      };
+      this.#validateDuplicateMenu(menuName);
+
+      this.#menuList.push(list);
+    });
+  }
+
+  #getMenuCategory(menuName) {
+    const menuCategory = Object.keys(MENU_LIST).find((category) =>
+      MENU_LIST[category].some(({ name }) => name === menuName),
+    );
+
+    return menuCategory;
+  }
+
+  #getMenuPrice(menuName) {
+    const menu = Object.values(MENU_LIST)
+      .flat()
+      .find((item) => item.name === menuName);
+
+    return menu.price;
+  }
+
+  #validateDuplicateMenu(menuName) {
+    if (this.#menuList.some(({ name }) => name === menuName)) {
+      throw new Error(ERROR.orderDuplicate);
+    }
+  }
+
+  #validateTotalCount() {
+    const totalCount = this.#menuList.reduce((total, { count }) => total + count, 0);
+
+    if (totalCount > 20) {
+      throw new Error(ERROR.order);
+    }
+  }
+
+  get totalPrice() {
+    return this.#menuList.reduce((total, { count, price }) => total + price * count, 0);
   }
 
   get menuList() {
